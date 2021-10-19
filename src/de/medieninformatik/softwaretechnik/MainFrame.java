8d20a58320ca8b9d627c462f3bdc76fadc6aa2a3
bxxmchen <u36651@hs-harz.de>
2021-10-19 23:55:13 +0200

    Version 1.1 two circles and menu for background color

--- src/de/medieninformatik/softwaretechnik/MainFrame.java
+++ src/de/medieninformatik/softwaretechnik/MainFrame.java
@@ -1,5 +1,6 @@
 package de.medieninformatik.softwaretechnik;
 
+import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -20,15 +21,49 @@ public class MainFrame extends Frame implements ActionListener{
 
     /**
      * The constructor of the class MainFrame sets the size, the title and the visibility.
+     * Furthermore it is responsible for the menu for the background color.
      */
     public MainFrame(){
         setSize(850, 550);
         setVisible(true);
-        setTitle("Version 1.0");
+        setTitle("Version 1.1");
 
         addWindowListener(mainFrameController);
         add(customCanvas);
         customCanvas.addMouseListener(canvasController);
+
+        MenuBar bar = new MenuBar();
+        Menu menuHg = new Menu("Hintergrund");
+
+        MenuItem bgWhite = new MenuItem("Weiß");
+        //background color changes to white
+        bgWhite.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                customCanvas.changeBackground(Color.WHITE);
+            }
+        });
+        MenuItem bgDarkgray = new MenuItem("Dunkelgrau");
+        //background color changes to dark gray
+        bgDarkgray.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                customCanvas.changeBackground(Color.DARK_GRAY);
+            }
+        });
+        MenuItem bgBlack = new MenuItem("Schwarz");
+        //background color changes to black
+        bgBlack.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                customCanvas.changeBackground(Color.BLACK);
+            }
+        });
+        menuHg.add(bgWhite);
+        menuHg.add(bgDarkgray);
+        menuHg.add(bgBlack);
+        bar.add(menuHg);
+        setMenuBar(bar);
 
     }
 
